df3e9b8a3b1f53d8b9f6bbfec5d0e73e69cf891c
Accoie <sypherjensqr@gmail.com>
2025-06-30 20:41:14 +0300

    final

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   base: "viteGitHubPages",
-  publicDir: 'public', // Убедитесь что указана правильная папка
+  publicDir: 'public', 
 });
